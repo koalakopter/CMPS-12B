32ffdf2b9786481211b5480ca5322ddc91c13f9b
Julian To <juliantomato123@outlook.com>
2018-10-01 15:51:41 -0700

    Added HelloUser2

---
+++ Lab 1/HelloUser2.java
@@ -0,0 +1,13 @@
+/*
+HelloUser2.java 
+by Julian To
+Simply prints out a greeting to the user
+*/
+
+class HelloUser2 {
+	public static void main( String[] args ){
+      String userName = System.getProperty("user.name");
+	  
+	  System.out.println("Hello " + userName);
+	  }
+}
