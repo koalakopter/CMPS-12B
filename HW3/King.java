b0627610968679093994676c7f78a4bd3505cf90
Julian To <juliantomato123@outlook.com>
2018-11-13 20:14:40 -0800

    GOnna need these

---
+++ HW3/King.java
@@ -0,0 +1,41 @@
+class King extends Chesspiece {
+	public King(int x, int y, boolean z) {
+		super(x, y, z);
+	}
+	
+	//God Save the King
+	public String giveName() {
+		if (this.colour) {
+			return "K";
+		} else
+			return "k";
+	}
+
+	public boolean isAttacking(Chesspiece origin) {
+		// king attacks one square around itself
+		// split up for debug purposes
+		if ((this.row + 1 == origin.row && this.col == origin.col)
+				|| (this.row - 1 == origin.row && this.col == origin.col)) {
+			// if the piece is the opposite colour, return true
+			if (this.colour != origin.colour) {
+				return true;
+			}
+		}
+		if ((this.col + 1 == origin.col && this.row == origin.row)
+				|| (this.col - 1 == origin.col && this.row == origin.row)) {
+			// if the piece is the opposite colour, return true
+			if (this.colour != origin.colour) {
+				return true;
+			}
+		}
+		// diagonal attack checks
+		if (((this.col + 1 == origin.col) && (this.row - 1 == origin.row || this.row + 1 == origin.row))
+				|| ((this.col - 1 == origin.col) && (this.row - 1 == origin.row || this.row + 1 == origin.row))) {
+			// if the piece is the opposite colour, return true
+			if (this.colour != origin.colour) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
---
+++ HW3/Queen.java
@@ -0,0 +1,61 @@
+class Queen extends Chesspiece {
+	public Queen(int x, int y, boolean z) {
+		super(x, y, z);
+	}
+
+	// this is da kween
+	public String giveName() {
+		if (this.colour) {
+			return "Q";
+		} else
+			return "q";
+	}
+
+	public boolean isAttacking(Chesspiece origin) {
+		// queens attack in all 8 directions
+		if (this.row == origin.row || this.col == origin.col) {
+			// if the piece is the opposite colour, return true
+			if (this.colour != origin.colour) {
+				return true;
+			}
+		}
+		int a, b;
+		// top right diagonal
+		for (a = this.row, b = this.col; a < 8 && b < 8; a++, b++) {
+			if (a == origin.checkRow() && b == origin.checkCol()) {
+				// if the piece is the opposite colour, return true
+				if (this.colour != origin.colour) {
+					return true;
+				}
+			}
+		}
+		// top left diagonal
+		for (a = this.row, b = this.col; a >= 0 && b < 8; a--, b++) {
+			if (a == origin.checkRow() && b == origin.checkCol()) {
+				// if the piece is the opposite colour, return true
+				if (this.colour != origin.colour) {
+					return true;
+				}
+			}
+		}
+		// bottom left diagonal
+		for (a = this.row, b = this.col; a >= 0 && b >= 0; a--, b--) {
+			if (a == origin.checkRow() && b == origin.checkCol()) {
+				// if the piece is the opposite colour, return true
+				if (this.colour != origin.colour) {
+					return true;
+				}
+			}
+		}
+		// bottom right diagonal
+		for (a = this.row, b = this.col; a < 8 && b >= 0; a++, b--) {
+			if (a == origin.checkRow() && b == origin.checkCol()) {
+				// if the piece is the opposite colour, return true
+				if (this.colour != origin.colour) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+}
