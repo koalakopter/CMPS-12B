3a2d77a4d0103ab7612a3cd51f6f60ff35c1aa09
Julian To <juliantomato123@outlook.com>
2018-11-14 16:01:29 -0800

    slowly chugging away

--- HW3/ChessMoves.java
+++ HW3/ChessMoves.java
@@ -1,23 +1,34 @@
+//Julian To
+//Jcto@ucsc.edu
+//HW 3: ChessMoves
+
+import java.io.*;
+
 public class ChessMoves {
 	public static void main(String[] args)
 	{
 		//me test me smart
 		LinkedList koala = new LinkedList();
-		Chesspiece one = new Rook(1, 1, true);
+		Chesspiece one = new Rook(7, 1, false);
 		Chesspiece two = new Bishop(2,2, false);
 		Chesspiece three = new Queen(3,3, true);
-		Chesspiece four = new Knight(4,3, false);
+		Chesspiece four = new Knight(6,5, true);
+		Chesspiece five = new King(7,7, true);
+		
 		koala.addNode(one);
 		koala.addNode(two);
 		koala.addNode(three);
 		koala.addNode(four);
+		koala.addNode(five);
+		
 		System.out.println(koala.print());
 		System.out.println(two.isAttacking(three));
 		//koala.delete(four);
 		//System.out.println(koala.print());
 		boolean test = koala.isLegalMove(three, 6, 6);
 		System.out.println("is it legal? " + test);
-		System.out.println("is something in de way? " + koala.inTheWay(three, 7, 3));
+		System.out.println("is something in de way? " + koala.inTheWay(two, 2, 2));
+		System.out.println("Is the king ok? " + koala.kingSafe(five));
 		
 	}
 }
