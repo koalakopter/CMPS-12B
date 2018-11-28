6f1b2689e76f697ed0df720458ee57a2f1428ac1
Julian To <juliantomato123@outlook.com>
2018-11-27 18:52:27 -0800

    first function down???

--- Lab5/queue.c
+++ Lab5/queue.c
@@ -3,4 +3,29 @@
 //JULIAN TO: JCTO
 //CMPS 12B
 
-#include queue.h
+#include "queue.h"
+
+#include <stdio.h>
+
+//FIRST FUNCTION: ENQUEUE
+char* enqueue(int x, List intList, char *output)
+{
+    //makes a new NodeItem item
+    //next item is null since its placed at the head
+    //second thing is just the int
+    struct NodeItem newData = {NULL, x};
+
+    //encodes the output
+    sprintf(output, "enqueued %d", x);
+    return output;
+}
+
+
+int main()
+{
+    int x = 5;
+    char output[50];
+    sprintf(output, "enqueued %d", x);
+    printf("%s",output);
+}
+
--- Lab5/queue.h
+++ Lab5/queue.h
@@ -8,3 +8,24 @@
 //DEQUEUE: DEQUEUES THE OLDEST INPUT AND REMOVES IT FROM THE LIST
 
 #endif // QUEUE_H_INCLUDED
+
+//this will contain each item in the list
+typedef struct NodeItem {
+    struct NodeItem *nextItem;
+    int data;
+} NodeItem;
+
+//this struct will contain the entire linked list
+typedef struct List
+{
+    struct nodeItem *head; //head of the list
+} List;
+
+
+//all these functions returns chars since we need to print stuff to an output file
+
+char* enqueue(int x, List intList, char *output); //adds an int to the list
+
+char* print(List intList, char *output); //prints out whatever is in the queue
+
+char* dequeue(List intList, char *output); //dequeues the oldest value, returns "empty" if empty
