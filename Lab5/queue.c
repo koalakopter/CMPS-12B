4373cdf326026b982d3863ede25aab9a9919e077
Julian To <juliantomato123@outlook.com>
2018-11-27 21:01:38 -0800

    enqueue might work now???

--- Lab5/queue.c
+++ Lab5/queue.c
@@ -7,10 +7,22 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
+//HELPER FUNCTIONS
+//creates a new list to be used
+List *newList()
+{
+    List *new = malloc(sizeof(List));
+    new->head = NULL;
+    return new;
+}
 
 //FIRST FUNCTION: ENQUEUE
-char* enqueue(int x, NodeItem *item, char* output)
+char* enqueue(int x, List *line, char* output)
 {
+    struct NodeItem *item;
+    item = line->head;
     //if this is the first item in the list, make a new list
     if(item == NULL)
     {
@@ -44,14 +56,16 @@ char* enqueue(int x, NodeItem *item, char* output)
     return output;
 }
 
-char* print(NodeItem *item, char* output)
+char* print(List *line, char* output)
 {
+    NodeItem *item = line->head;
     //char output[900]; //I hope the list never gets this long lmao
     //if the list is empty, print nothing
     if (item == NULL)
     {
         sprintf(output, "");
     }
+    printf("here?");
     //else go to the front of the list
     while((item->prevItem) != NULL)
     {
@@ -62,6 +76,7 @@ char* print(NodeItem *item, char* output)
     //once at the front, just print out the elements
     while((item->nextItem) != NULL)
     {
+        printf("%d ", item->data);
         sprintf(temp, "%d ", item->data);
         strcat(output, temp);
         item->nextItem;
@@ -82,8 +97,12 @@ int main()
     int x = 50;
     int y = 40;
     char output[69];
-    sprintf(output, enqueue(x, NULL, output));
-    sprintf(output, enqueue(x, NULL, output));
-    printf("%s", output);
+    List *myList = newList();
+    sprintf(output, enqueue(x, myList, output));
+    printf("%s\n", output);
+    sprintf(output, enqueue(y, myList, output));
+    printf("%s\n", output);
+    sprintf(output, print(myList, output));
+
 }
 
