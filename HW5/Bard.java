8bfec43163ea96a05a82bba380eada7650e20f4b
Julian To <juliantomato123@outlook.com>
2018-12-03 19:15:38 -0800

    Added a new word class

--- HW5/Bard.java
+++ HW5/Bard.java
@@ -8,20 +8,13 @@ import java.io.*;
 import java.util.*;
 
 public class Bard {
-	//processes the String array
-	//turns it into an Array List
-	public static String[] process(String input)
-	{
-		
-		
-		return output;
-	}
+
 	//main function that parses strings
 	public static void main(String[] args) throws IOException {
 
 		// check number of command line arguments is at least 2
 
-		
+	
 		if (args.length < 2) {
 			System.out.println("Usage: java -jar Bard.jar <input file> <output file>");
 			System.exit(1);
@@ -33,12 +26,29 @@ public class Bard {
 		Scanner shakespeare = new Scanner(new File("shakespeare.txt"));
 		PrintWriter out = new PrintWriter(new FileWriter(args[1]));
 		
+		//new Hashtables
+		//one stores frequency, the other the length
+		Hashtable <String, Integer> frequency = new Hashtable<String, Integer> ();
+		Hashtable <String, Integer> length = new Hashtable<String, Integer> ();
+		
+		//ArrayList of word objects
+		ArrayList <Word> words = new ArrayList<Word>();
+		
 		//first process the entire shakespeare.txt, turning it into a String array
 		while (shakespeare.hasNextLine() == true)
 		{
 			//like reading the input, but with shakespeare instead
 			String shake_line = shakespeare.nextLine().trim() + " "; 
-			String shake_token = line.split()
+			 //split around punctuation and white space
+			String[] shake_token = shake_line.split("[\\s\\?\\,\\.\\!\\:\\;\\[\\]]+");
+			
+			//lowercases all tokens
+			//and puts them into an Array List for good measure?
+			for (String x : shake_token)
+			{
+				x = x.toLowerCase();
+				
+			}
 		}
 		
 		// read lines from in, extract and print tokens from each line
@@ -65,11 +75,20 @@ public class Bard {
 		// close files
 		in.close();
 		out.close();
+		shakespeare.close();
 		
 	
 		/*
 		// TESTING CORNER
-
+			String[] token = new String[3];
+			token[0] = "FUGG";
+			token[1] = "KOALA";
+			token[2] = "MEME";
+			for(String x : token)
+			{
+				x = x.toLowerCase();
+				System.out.println(x);
+			}
 		*/
 	}
 	
---
+++ HW5/Word.java
@@ -0,0 +1,11 @@
+//word object that stores a word
+class Word {
+	String phrase; //the word stored
+	int frequency; //the frequency of the word
+	
+	//constructor
+	public Word(String input)
+	{
+		this.phrase = input;
+	}
+}
